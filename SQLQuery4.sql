30a84cd8a4c827d40732cbaca80124032152cdea
sthewar.rojano <sthewar.rojano@grupomok.com>
2022-05-23 16:48:37 -0500

    ajustes iniciales

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,11 +1,29 @@
-USE [dbPrueba]
-GO
+create database dbPrueba
+use dbPrueba
 
-INSERT INTO [dbo].[usuario]
-           ([correo]
-           ,[clave])
-     VALUES
-           ('sthewar','sthewar')
-GO
+create table estado(
+id int identity(1,1) primary key,
+titulo varchar(100)
+)
+create table rol(
+id int identity(1,1) primary key,
+titulo varchar(100)
+)
+create table usuario(
+id int identity(1,1) primary key,
+correo varchar(100),
+clave varchar(max),
+estado int,
+rol int,
+FOREIGN KEY (rol) REFERENCES rol(id),
+FOREIGN KEY (estado) REFERENCES estado(id)
+)
 
-
+create table productoSlide(
+id int identity(1,1) primary key,
+titulo varchar(100),
+descripcion varchar(max),
+imagenes varchar(max),
+estado int,
+FOREIGN KEY (estado) REFERENCES estado(id),
+)
